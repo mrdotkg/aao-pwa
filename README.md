78fdf7048d3cf46b2582a2dc2ccd154a6f497be2
Kumar Gaurav <mrdotkg@butterops.Dlink>
2021-03-16 00:44:45 +0530

    pwa + ui

---
+++ README.md
@@ -0,0 +1,35 @@
+
+# Peridot
+A fully featured Vue 2.6 PWA boilerplate
+![Screenshot](./docs/readme_resources/homepage.png)
+
+## Features:
+* Progressive Web App (PWA) with Service Worker
+* Code splitting and async component/route loading
+* Automatic hot-reload in development
+* Dockerized production setup with [pm2 load balancing](https://github.com/Unitech/pm2)
+* Automatic removal of unused CSS/SCSS
+* Global SCSS variables and easy theme customization
+* Linting and formatting with [eslint](https://github.com/eslint/eslint) and [prettier](https://github.com/prettier/prettier)
+* Uses [Buefy](https://buefy.github.io/#/) for styling
+
+## Running
+```bash
+# Run in development
+yarn run serve
+
+# Building and running with Docker
+docker build -t peridot .
+docker run -p 8080:8080 peridot:latest
+
+# Lint .vue, .js, .scss
+yarn run lint
+```
+
+## Backend
+
+Want a ready-to-go backend for this web app for user authentication and management, email verification and more? Check out [microAuth](https://github.com/gingernaut/microAuth), a FastAPI Python boilerplate that works out of the box with this project.
+
+## Contributing
+
+Pull requests are welcome and appreciated!
---
+++ src/views/index-page.vue
@@ -0,0 +1,190 @@
+<template>
+  <div>
+    <section class="section">
+      <div class="container">
+        <div class="columns is-mobile">
+          <div class="column">
+            <h1 class="title is-size-2">🄰🄰🄾</h1>
+            <h2 class="subtitle is-size-5">𝗟𝗲𝘁❜𝘀 𝗽𝗹𝗮𝘆 𝗳𝗲𝘄 𝗴𝗮𝗺𝗲𝘀 𝘁𝗼𝗱𝗮𝘆.</h2>
+            <div class="buttons">
+              <a class="button is-danger is-small" href="#">JOIN & BOOK SLOTS FREE</a>
+              <a class="button is-small" href="#">READ FAQs</a>
+            </div>
+          </div>
+        </div>
+        <ul>
+          <li>
+            <img
+              src="https://via.placeholder.com/50/FFDD57/000000?text=%2b"
+            ><img
+              src="https://via.placeholder.com/50/FFDD57/000000?text=%2b"
+            ><img
+              src="https://via.placeholder.com/50/FFDD57/000000?text=%2b"
+            ><img src="https://via.placeholder.com/50/FFDD57/000000?text=%2b" >
+          </li>
+          <li>
+            <img
+              src="https://via.placeholder.com/50/FFDD57/000000?text=%2b"
+            ><img
+              src="https://via.placeholder.com/50/FFDD57/000000?text=%2b"
+            ><img
+              src="https://via.placeholder.com/50/FFDD57/000000?text=%2b"
+            ><img src="https://via.placeholder.com/50/FFDD57/000000?text=%2b" >
+          </li>
+          <li>
+            <img
+              src="https://via.placeholder.com/50/FFDD57/000000?text=%2b"
+            ><img
+              src="https://via.placeholder.com/50/FFDD57/000000?text=%2b"
+            ><img
+              src="https://via.placeholder.com/50/FFDD57/000000?text=%2b"
+            ><img src="https://via.placeholder.com/50/FFDD57/000000?text=%2b" >
+          </li>
+          <li>
+            <img
+              src="https://via.placeholder.com/50/FFDD57/000000?text=%2b"
+            ><img
+              src="https://via.placeholder.com/50/FFDD57/000000?text=%2b"
+            ><img
+              src="https://via.placeholder.com/50/FFDD57/000000?text=%2b"
+            ><img src="https://via.placeholder.com/50/FFDD57/000000?text=%2b" >
+          </li>
+          <li>
+            <img
+              src="https://via.placeholder.com/50/FFDD57/000000?text=%2b"
+            ><img
+              src="https://via.placeholder.com/50/FFDD57/000000?text=%2b"
+            ><img
+              src="https://via.placeholder.com/50/FFDD57/000000?text=%2b"
+            ><img src="https://via.placeholder.com/50/FFDD57/000000?text=%2b" >
+          </li>
+          <li>
+            <img
+              src="https://via.placeholder.com/50/FFDD57/000000?text=%2b"
+            ><img
+              src="https://via.placeholder.com/50/FFDD57/000000?text=%2b"
+            ><img
+              src="https://via.placeholder.com/50/FFDD57/000000?text=%2b"
+            ><img src="https://via.placeholder.com/50/FFDD57/000000?text=%2b" >
+          </li>
+          <li>
+            <img
+              src="https://via.placeholder.com/50/FFDD57/000000?text=%2b"
+            ><img
+              src="https://via.placeholder.com/50/FFDD57/000000?text=%2b"
+            ><img
+              src="https://via.placeholder.com/50/FFDD57/000000?text=%2b"
+            ><img src="https://via.placeholder.com/50/FFDD57/000000?text=%2b" >
+          </li>
+          <li>
+            <img
+              src="https://via.placeholder.com/50/FFDD57/000000?text=%2b"
+            ><img
+              src="https://via.placeholder.com/50/FFDD57/000000?text=%2b"
+            ><img
+              src="https://via.placeholder.com/50/FFDD57/000000?text=%2b"
+            ><img src="https://via.placeholder.com/50/FFDD57/000000?text=%2b" >
+          </li>
+          <li>
+            <img
+              src="https://via.placeholder.com/50/FFDD57/000000?text=%2b"
+            ><img
+              src="https://via.placeholder.com/50/FFDD57/000000?text=%2b"
+            ><img
+              src="https://via.placeholder.com/50/FFDD57/000000?text=%2b"
+            ><img src="https://via.placeholder.com/50/FFDD57/000000?text=%2b" >
+          </li>
+          <li>
+            <img
+              src="https://via.placeholder.com/50/FFDD57/000000?text=%2b"
+            ><img
+              src="https://via.placeholder.com/50/FFDD57/000000?text=%2b"
+            ><img
+              src="https://via.placeholder.com/50/FFDD57/000000?text=%2b"
+            ><img src="https://via.placeholder.com/50/FFDD57/000000?text=%2b" >
+          </li>
+        </ul>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Index",
+  components: {},
+  mixins: [],
+  data() {
+    return {
+      data: [
+        {
+          id: 1,
+          first_name: "DOT",
+          last_name: "GHOST",
+          date: "NINJA",
+          gender: "KNIG",
+        },
+        {
+          id: 2,
+          first_name: "PAN",
+          last_name: "TIN",
+          date: "SEN",
+          gender: "GOL",
+        },
+        {
+          id: 3,
+          first_name: "GOU",
+          last_name: "TIH",
+          date: "NOJ",
+          gender: "LPK",
+        },
+        {
+          id: 4,
+          first_name: "KIJ",
+          last_name: "AWS",
+          date: "RTG",
+          gender: "NHJ",
+        },
+        {
+          id: 5,
+          first_name: "IJK",
+          last_name: "LKJ",
+          date: "WER",
+          gender: "GHJ",
+        },
+      ],
+      columns: [
+        {
+          field: "first_name",
+          // label: "P#1",
+        },
+        {
+          field: "last_name",
+          // label: "P#2",
+        },
+        {
+          field: "date",
+          // label: "P#3",
+          centered: true,
+        },
+        {
+          field: "gender",
+          // label: "P#4",
+        },
+      ],
+    }
+  },
+  beforeCreate() {},
+  created() {},
+  beforeMount() {},
+  mounted() {},
+  computed: {},
+  methods: {},
+  filters: {},
+  beforeUpdate() {},
+  updated() {},
+  beforeDestroy() {},
+}
+</script>
+
+<style lang="scss" scoped></style>
